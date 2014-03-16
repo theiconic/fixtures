4d620670e5868a9b3cfe887d5188c7bd4cda3693
George Borges <george.borges@theiconic.com.au>
2014-03-17 10:14:23 +1100

    Yaml return fixture collection

--- src/Parser/YamlParser.php
+++ src/Parser/YamlParser.php
@@ -2,14 +2,17 @@
 
 namespace TheIconic\Fixtures\Parser;
 
+use TheIconic\Fixtures\Fixture\FixtureCollection;
 use Symfony\Component\Yaml\Yaml;
 
 class YamlParser extends AbstractParser
 {
-    protected  static $parsableExtension = '.yml';
+    protected static $parsableExtension = '.yml';
 
     public function parse($source)
     {
-        return Yaml::parse(file_get_contents($source));
+        $fixtureArray = Yaml::parse(file_get_contents($source));
+
+        return FixtureCollection::create($fixtureArray);
     }
 }
