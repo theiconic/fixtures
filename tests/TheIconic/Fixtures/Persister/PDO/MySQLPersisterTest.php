39401da12341efea6501f49e8cb2864faf3a9ee7
George Borges <george.borges@theiconic.com.au>
2014-03-30 01:49:01 +1100

    Adding unit tests for MySQL persister

---
+++ tests/TheIconic/Fixtures/Persister/PDO/MySQLPersisterTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace TheIconic\Fixtures\Persister\PDO;
+
+use TheIconic\Fixtures\Fixture\Fixture;
+
+class MySQLPersisterTest extends \PHPUnit_Framework_TestCase
+{
+    private $testParsedData = [
+        'country' => [
+            ['id_country' => 1, 'iso2_code' => 'AU', 'iso3_code' => 'AUS', 'name' => 'Australia'],
+            ['id_country' => 2, 'iso2_code' => 'VE', 'iso3_code' => 'VEN', 'name' => 'Venezuela'],
+        ]
+    ];
+
+    /**
+     * @var Fixture
+     */
+    private $testFixture;
+
+    public function setUp()
+    {
+        $this->testFixture = Fixture::create($this->testParsedData);
+    }
+
+    public function testPersist()
+    {
+        $persister = new MySQLPersister(
+            $_ENV['host'],
+            $_ENV['database'],
+            $_ENV['username'],
+            $_ENV['password']
+        );
+
+        $this->assertTrue($persister->persist($this->testFixture));
+        $this->assertTrue($persister->close());
+    }
+
+    public function testCleanStorage()
+    {
+        $persister = new MySQLPersister(
+            $_ENV['host'],
+            $_ENV['database'],
+            $_ENV['username'],
+            $_ENV['password']
+        );
+
+        $persister->persist($this->testFixture);
+        $this->assertTrue($persister->cleanStorage());
+        $this->assertTrue($persister->close());
+    }
+
+    /**
+     * @expectedException \TheIconic\Fixtures\Exception\PersisterException
+     */
+    public function testInvalidConnection()
+    {
+        $persister = new MySQLPersister(
+            $_ENV['host'],
+            $_ENV['database'],
+            $_ENV['username'],
+            'fake'
+        );
+
+        $persister->persist($this->testFixture);
+        $this->assertTrue($persister->close());
+    }
+}
---
+++ tests/TheIconic/Fixtures/Persister/PDO/PersisterFactoryTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace TheIconic\Fixtures\Persister\PDO;
+
+class PersisterFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+        $persister = PersisterFactory::create(
+            $_ENV['host'],
+            $_ENV['database'],
+            $_ENV['username'],
+            $_ENV['password'],
+            'mysql'
+        );
+
+        $this->assertInstanceOf('TheIconic\Fixtures\Persister\PDO\MySQLPersister', $persister);
+    }
+
+    /**
+     * @expectedException \TheIconic\Fixtures\Exception\PersisterException
+     */
+    public function testInvalidCreate()
+    {
+        PersisterFactory::create(
+            $_ENV['host'],
+            $_ENV['database'],
+            $_ENV['username'],
+            $_ENV['password'],
+            'fakedb'
+        );
+    }
+}
