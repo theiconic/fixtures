2a68032fadc976f197b075e191509cc517ac1d7a
switkowski <sebastian@homemail.com>
2016-01-14 16:12:32 +0100

    Small refactor. Fixes code coverage.

--- src/Parser/XmlEmptiableParser.php
+++ src/Parser/XmlEmptiableParser.php
@@ -2,6 +2,7 @@
 
 namespace TheIconic\Fixtures\Parser;
 
+use TheIconic\Fixtures\Exception\InvalidParserException;
 use TheIconic\Fixtures\Fixture\Fixture;
 
 /**
@@ -22,46 +23,20 @@ class XmlEmptiableParser extends XmlParser
      *
      * @param $source
      * @return Fixture
-     * @throws \TheIconic\Fixtures\Exception\InvalidParserException
      */
     public function parse($source)
     {
-        $fixtureArray = [];
-        $z = new \XMLReader;
-        $z->open($source);
+        try {
+            return parent::parse($source);
+        } catch (InvalidParserException $e) {
+            $z = new \XMLReader;
+            $z->open($source);
 
-        $doc = new \DOMDocument;
-
-        while ($z->read() && $z->name !== 'table_data');
-        $tableName = $z->getAttribute('name');
-
-        $rowNum = 0;
-        while ($z->read() && $z->name !== 'row');
-        while ($z->name === 'row')
-        {
-            $node = simplexml_import_dom($doc->importNode($z->expand(), true));
-
-            $totalAttributes = $node->count();
-            for ($i = 0; $i < $totalAttributes; $i++) {
-                foreach ($node->field[$i]->attributes() as $attribute) {
-                    $attribute = (string) $attribute;
-                    $value = (string) $node->field[$i];
-                    $namespaces = $node->field[$i]->getNamespaces(true);
-
-                    if (empty($namespaces)) {
-                        $fixtureArray[$tableName][$rowNum][$attribute] = $value;
-                    }
-                }
-            }
-
-            $rowNum++;
-            $z->next('row');
-        }
-
-        if (empty($fixtureArray)) {
+            while ($z->read() && $z->name !== 'table_data');
+            $tableName = $z->getAttribute('name');
             $fixtureArray[$tableName] = [];
-        }
 
-        return Fixture::create($fixtureArray);
+            return Fixture::create($fixtureArray);
+        }
     }
 }
--- tests/TheIconic/Fixtures/Parser/XmlEmptiableParserTest.php
+++ tests/TheIconic/Fixtures/Parser/XmlEmptiableParserTest.php
@@ -4,7 +4,7 @@ namespace TheIconic\Fixtures\Parser;
 
 class XmlEmptiableParserTest extends \PHPUnit_Framework_TestCase
 {
-    const TESTS_FIXTURES_DIRECTORY = './fixtures/tests/Support/TestsFixtures/';
+    const TESTS_FIXTURES_DIRECTORY = './tests/Support/TestsFixtures/';
 
     /**
      * @var XMLEmptiableParser
@@ -16,13 +16,24 @@ class XmlEmptiableParserTest extends \PHPUnit_Framework_TestCase
         $this->parserInstance = new XmlEmptiableParser();
     }
 
-    public function testXmlEmptiableParser()
+    public function testXmlEmptiableParserShouldReturnEmptyFixtureWhenEmptyXmlIsSpecified()
+    {
+        $fixtureFile = self::TESTS_FIXTURES_DIRECTORY . 'currency_conversion_no_rows.empty.xml';
+
+        $fixture = $this->parserInstance->parse($fixtureFile);
+
+        $this->assertCount(0, $fixture);
+        $this->assertEquals('currency_conversion', $fixture->getName());
+        $this->assertInstanceOf('TheIconic\Fixtures\Fixture\Fixture', $fixture);
+    }
+
+    public function testXmlEmpitableParserShouldReturnFixtureWithDataWhenNonEmptyXmlIsSpecified()
     {
         $fixtureFile = self::TESTS_FIXTURES_DIRECTORY . 'currency_conversion.empty.xml';
 
         $fixture = $this->parserInstance->parse($fixtureFile);
 
-        $this->assertCount(0, $fixture);
+        $this->assertCount(33, $fixture);
         $this->assertEquals('currency_conversion', $fixture->getName());
         $this->assertInstanceOf('TheIconic\Fixtures\Fixture\Fixture', $fixture);
     }
